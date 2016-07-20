a1fb9b646f7866ca78d99b7d5300cc193891e0a6
TGAStudent <tga.student@telegraphacademy.com>
2016-07-20 14:56:49 -0700

    fixed typo in server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var app = require('./server-config.js');
 
 var port = 8080;
 
-app.listen(process.end.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 
 //console.log('Server now listening on port ' + port);
